e98c1dd91e1ba83fa6e1a01d8de856d995b99cce
Jacques <jacquescoetzee91@gmail.com>
2020-11-02 17:14:42 +0000

    exc2 Q15

---
+++ 02-arrays-functions/15-total.php
@@ -0,0 +1,29 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function total($items) {
+    $total = null;
+    foreach($items as $item){
+        $total += $item["price"];
+    };
+    return $total;
+};
+
+$shoppingList = [[
+    "name" => "coffee",
+    "price" => 3.50,
+], [
+    "name" => "tea",
+    "price" => 2.50,
+], [
+    "name" => "toblerone",
+    "price" => 3.20,
+], [
+    "name" => "newspaper",
+    "price" => 0.10,
+]];
+
+dump(
+    total($shoppingList), // 9.3
+);
