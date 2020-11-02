b5add02993711d4769bcba55f8cf5b2b3d2ab696
Jacques <jacquescoetzee91@gmail.com>
2020-11-02 16:45:58 +0000

    exc2 Q11

---
+++ 02-arrays-functions/11-double-collect.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function double($numArr){
+    return collect($numArr)->map(fn($num) => $num + $num);
+};
+
+
+
+dump(
+    double([2, 3, 4, 5, 6]), // [4, 6, 8, 10, 12]
+    double([1, 2, 5]), // [2, 4, 10]
+);
