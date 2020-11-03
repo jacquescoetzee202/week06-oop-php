7be555efc70b9ca2c932be29be77dcc3dea634e4
Jacques <jacquescoetzee91@gmail.com>
2020-11-03 14:21:59 +0000

    exc3 Q5

---
+++ 03-regex/05-words.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function words($sentance){
+    return preg_split("/[,\s]+/",$sentance);
+};
+
+dump(words("This is a sentence")); // ["This", "is", "a", "sentence"]
+dump(words("Hello, my name is Dolemite")); // ["Hello", "my", "name", "is", "Dolemite"]
